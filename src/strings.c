310d7d7385da7d945fef2bb64541de898dd6f014
Your Name <you@example.com>
2021-08-21 19:49:53 -0400

    Fixed some bugs in strings.c as well as some comments that didn't match up with the code

--- src/strings.c
+++ src/strings.c
@@ -1,4 +1,5 @@
 #include <stdio.h> // Required for functions like strcpy and strcat
+#include <string.h> // Required for functions like strcpy_s and strcat_s 
 
 // There is no built in special class for strings. Instead, strings are just
 // **sequences of characters that occupy contiguous memory locations**
@@ -6,14 +7,16 @@
 int main(void)
 {
     // C# equivalent: string name = "Connie";
-    char name[] = "Connie";
+    char sampleName[] = "Connie";
     // All strings in C are null terminated, so the string "Connie" will occupy 
     // 7 bytes in memory - one for each letter and then the null char \0
     // Without the null char, if you attempted to read the string from memory, it 
     // would just keep going into the next spaces, regardless of what data type they really represent
 
+    printf("What is your name?\n");
+
     // To read a string from the console:
-    char inputName[40]; // Has 39 chars for input and 1 for null
+    char name[40]; // Has 39 chars for input and 1 for null
     scanf("%s", name);
 
     // The string format specifier "%s" can be changed to "%39s" to protect against bad input
@@ -24,37 +27,51 @@ int main(void)
     // It's almost like the & is already included in the array name.
 
     // Printing a string:
-    printf("Nice to meet you, %s", inputName);
+    printf("Nice to meet you, %s.\n", name);
 
     // Copying strings (unsafe):
     char source[] = "source";
-    char destination[100];
-    strcpy(destination, source);
+    char unsafeDestination[100];
+    strcpy(unsafeDestination, source);
 
     // Concatenating strings (unsafe):
     // Appends source to destination
-    strcat(destination, source);
+    strcat(unsafeDestination, source);
 
     // To get the string "{inputName} is learning C.", instead of doing something like
     // var message = inputName + " is " + "learning C."
     // You would do:
-    char message[100];
-    strcpy(message, inputName);
-    strcpy(message, " is ");
-    strcpy(message, "learning C.");
+    char unsafeMessage[100];
+    // Initializing a string to empty right after allocation can help prevent
+    // weird chars from being prepended to it
+    unsafeMessage[0] = '\0';
+    strcat(unsafeMessage, name);
+    strcat(unsafeMessage, " is ");
+    strcat(unsafeMessage, "learning C.");
 
     // These functions are UNSAFE.
     // The safe versions have more runtime checks for things like buffer overflow
-    
-    // Safe string functions require destination buffer size as an argument:
+    // Safe string functions require destination buffer size as an argument.
 
     // Copying strings (safe):
-    strcpy_s(message, 100, inputName);
-    strcpy_s(message, 100, " is ");
-    strcpy_s(message, 100, "learning C.");
+    char safeDestination[100];
+    strcpy_s(safeDestination, 100, source);
+
+    // Concatenating strings (safe):
+    char safeMessage[100];
+    safeMessage[0] = '\0';
+    strcat_s(safeMessage, 100, name);
+    strcat_s(safeMessage, 100, " is ");
+    strcat_s(safeMessage, 100, "learning C.");
     // Alternative, we can use a variable for the buffer size:
-    strcpy_s(message, sizeof(message), "whatever");
+    strcat_s(safeMessage, sizeof(safeMessage), "\nGoodbye.");
 
     // These safe versions stop writing characters when the buffer size is reached
     // Yay, no overflows! ＼(＾O＾)／
+
+    // Puts is an alternative to printf that doesn't take any formatting
+    // It also appends a \n at the end
+    puts(safeMessage);
+
+    return 0;
 }
