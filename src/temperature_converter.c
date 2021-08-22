8bcbf51b1053db976417ee3f071417b7815f3184
Lauren Rutledge <l.rutledge138@gmail.com>
2021-08-22 11:50:15 -0400

    Started modifications to temperature conversion code to use functions, mid-bug fix

--- src/temperature_converter.c
+++ src/temperature_converter.c
@@ -2,19 +2,46 @@
 
 int main(void) 
 {
-    // Essentially Console.WriteLine()
-    printf("Please enter a temperature value in Fahrenheit: ");
-    float temperatureInFahrenheit;
-    // Console.ReadLine()
-    // "%f" is a format specifier that tells the compiler what type of data to expect from the user
-    // & means "address of" (is a pointer) that tells scanf where the variable is located in memory
-    // &temperatureInFahrenheit == "Write the value that's read from the user to memory location 0x0064C0A1 (example)"
-    scanf("%f", &temperatureInFahrenheit);
-    float temperatureInCelsius = (temperatureInFahrenheit - 32) * 5 / 9; 
-    // Similar to string.Format
-    // The %f format specifier tells the compiler that the output variable is a float
-    // The .2 in "%.2f" specifies that we only want two decimal places to be shown
-    printf("The corresponding temperature in Celsius is %.2f C. \n", temperatureInCelsius);
+    /*
+        // Essentially Console.WriteLine()
+        printf("Please enter a temperature value in Fahrenheit: ");
+        float temperatureInFahrenheit;
+        // Console.ReadLine()
+        // "%f" is a format specifier that tells the compiler what type of data to expect from the user
+        // & means "address of" (is a pointer) that tells scanf where the variable is located in memory
+        // &temperatureInFahrenheit == "Write the value that's read from the user to memory location 0x0064C0A1 (example)"
+        scanf("%f", &temperatureInFahrenheit);
+        float temperatureInCelsius = ConvertToCelcius(temperatureInFahrenheit);
+        // Similar to string.Format
+        // The %f format specifier tells the compiler that the output variable is a float
+        // The .2 in "%.2f" specifies that we only want two decimal places to be shown
+        printf("The corresponding temperature in Celsius is %.2f C. \n", temperatureInCelsius);
+    */
+
+    // Print table header
+    puts("Temperature Conversion Table");
+    puts("============================");
     
+    // Print table values, one row at a time
+    for (float tempInF = 10.0; tempInF < 100.0; tempInF += 5.0)
+    {
+        float tempInC = ConvertToCelcius(tempInF);
+        // .1 limits decimal places to 1 digit, 5.1 does same as well as 
+        // specifying the number(s) before the digit has to occupy 5 spaces
+        //printf(" %.1f F     |     %5.1f C \n", tempInF, tempInC);
+        printf("%f      ", 1.00);
+        printf("%f \n", tempInC);
+    }
+    
+    puts("----------------------------");
+
     return 0;
+}
+
+// This is considered the "function prototype/declaration" instead of C#'s "method signature"
+float ConvertToCelcius(float temperatureInFahrenheit) 
+{
+    //float temperatureInCelsius = (temperatureInFahrenheit - 32.0) * 5.0 / 9.0;
+    //return temperatureInCelsius;
+    return 17.0;
 }
